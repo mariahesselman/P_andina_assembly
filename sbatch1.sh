ab858fa3b5d1eb11a57cce1dd8d22fc5270690a3
boeydaryl <boeydaryl@gmail.com>
2018-11-29 12:49:39 +0100

    modified bash script for MEGAHIT assembler

--- sbatch1.sh
+++ sbatch1.sh
@@ -2,12 +2,8 @@
 #Template 1 for assembly
 
 module load bioinfo-tools
-module load bwa
-module load abyss
+module load megahit
 
-cd Raw_Data
+cd /home/zhong/PROJECT/P_andina_assembly/Raw_Data
 
-abyss-pe k=96 name=testassembly lr='lra' lra='SRR1817238.fastq'
-
-
-done
+megahit -r SRR1817238.fastq -o megahit_out
